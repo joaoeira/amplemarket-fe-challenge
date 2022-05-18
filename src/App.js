11440e8ac30d8c8892ecc504fa7892048e1ef5ff
João Eira <simbyotic@gmail.com>
2022-05-18 20:36:04 +0100

    small tweak

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ export default function Example() {
         <div className="py-10">
           <header>
             <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-              <h1 className="text-3xl font-bold leading-tight text-gray-900">Gotta Catch 'Em All</h1>
+              <h1 className="text-3xl font-bold leading-tight text-gray-900">Pokédex</h1>
             </div>
           </header>
           <main>
--- src/components/PokeContainer.jsx
+++ src/components/PokeContainer.jsx
@@ -6,15 +6,15 @@ import PokeCard from './PokeCard/PokeCard';
 function Container() {
   return (
     <>
-    <TextInput
-    placeholder="Pokemon Name"
-    label="Pokemon Name"
-  />
+      <TextInput
+      placeholder="Pokemon Name"
+      label="Pokemon Name"
+    />
 
-<div style={{  display: "grid", gridTemplateColumns: "auto auto auto",padding: "10px"}}>
-  {/* <PokeCard pokemon={ insert pokemon here} /> */}
-</div>
-  </>
+    <div style={{  display: "grid", gridTemplateColumns: "auto auto auto",padding: "10px"}}>
+      {/* <PokeCard pokemon={ poke} /> */}
+    </div>
+    </>
   );
 }
 
